dc3c6bbaf453f4ff931a7c2d20fb9ddadbc63fe7
scoutfrfr <scoutbotha@gmail.com>
2026-05-05 17:20:00 +0100

    Added weapon
    Added attack animation and sound effect, is triggered by pressing B.

--- Coursework/CMP105App/Player.h
+++ Coursework/CMP105App/Player.h
@@ -37,6 +37,7 @@ private:
     Animation m_idle;
     Animation m_walk;
     Animation m_sprint;
+    Animation m_attack;
     sf::Vector2f m_accel;
     float m_sprintTimer = 0.f;
     bool m_isGrounded;
@@ -44,15 +45,18 @@ private:
     float m_rightEdge;
     float m_playerHealth = 3.0f;
     float m_invincibilityDuration = 1.5f;
+    float m_attackDuration = 1.f;
     sf::Vector2f m_leverPosition;
     sf::Vector2f m_endPosition;
     sf::Clock invincibilityClock;
+    sf::Clock attackClock;
     bool m_leverPulled = false;
     bool m_gameEndTriggered = false;
     bool m_canDoubleJump;
     bool m_hasDoubleJumped;
     bool m_playerDead = false;
     bool m_isInvincible = false;
+    bool m_isAttacking = false;
     AudioManager* m_audio;
 
     const float SPRINT_COOLDOWN = 2.0f;
