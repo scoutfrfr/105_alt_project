badec6f360fb4ebacdc17f9a047c09ddc61814e3
scoutfrfr <scoutbotha@gmail.com>
2026-05-05 18:46:23 +0100

    Update Player.h

--- Coursework/CMP105App/Player.h
+++ Coursework/CMP105App/Player.h
@@ -58,7 +58,7 @@ private:
     bool m_playerDead = false;
     bool m_isInvincible = false;
     bool m_isAttacking = false;
-    bool m_weaponUnlocked = true;
+    bool m_weaponUnlocked = false;
     AudioManager* m_audio;
 
     const float SPRINT_COOLDOWN = 2.0f;
